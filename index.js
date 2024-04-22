6aef2761dc0e694853940b18c5331d33d8650c77
NewMiniBoss <gabriel.f.lazari@gmail.com>
2024-04-21 21:51:50 -0300

    default null params

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 "use strict";
 class CreateEmployee {
-    constructor(name, profession) {
+    constructor(name, profession, address = null, zipCode = null) {
         this.name = name;
         this.profession = profession;
+        this.address = address;
+        this.zipCode = zipCode;
     }
     getEmployee() {
-        return { name: this.name, profession: this.profession };
+        return {
+            name: this.name,
+            profession: this.profession,
+            address: this.address,
+            zipCode: this.zipCode
+        };
     }
-    ;
 }
 const employees = [];
 employees.push(new CreateEmployee('Gabriel', 'Software Engineer').getEmployee());
--- index.ts
+++ index.ts
@@ -1,15 +1,25 @@
 class CreateEmployee {
-    constructor(public name: string, public profession: string) {}
+    constructor( 
+        public name: string,
+        public profession: string,
+        public address: string | null = null,
+        public zipCode: string | null = null
+    ) {}
 
-    public getEmployee(): object { 
-        return { name: this.name, profession: this.profession };
-    };
+    getEmployee(): object {
+        return {
+            name: this.name,
+            profession: this.profession,
+            address: this.address,
+            zipCode: this.zipCode
+        };
+    }
 }
 
 const employees = [];
 
 employees.push(new CreateEmployee('Gabriel', 'Software Engineer').getEmployee());
 employees.push(new CreateEmployee('Tainá', 'Software Engineer').getEmployee());
-employees.push(new CreateEmployee('Marcos', 'Software Engineer').getEmployee());
+employees.push(new CreateEmployee('Marcos', 'Software Engineer').getEmployee())
 
 console.log(employees);
